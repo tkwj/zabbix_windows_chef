0474eb6f69347acdaf73d653c535e3b489020b8a
H Waldo G <gwaldo@gmail.com>
2012-11-16 13:39:32 -0500

    correcting metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Ernst Publishing, Inc."
-maintainer_email "waldog@ernstinfo.com"
+maintainer       'H. "Waldo" Grunenwald'
+maintainer_email "gwaldo@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures zabbix client on Windows."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
