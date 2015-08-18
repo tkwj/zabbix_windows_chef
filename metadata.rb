8958c3d3ed50b566e39faf891b9931a489ee6ad3
Thomas Vincent <thomasvincent@gmail.com>
2015-08-17 23:12:07 -0700

    Updating metadata after adopting cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       'H. "Waldo" Grunenwald'
-maintainer_email "gwaldo@gmail.com"
+maintainer       'Thomas Vincent'
+maintainer_email "thomasvincent@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures zabbix client on Windows."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
